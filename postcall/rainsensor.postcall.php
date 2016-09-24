e823008c0171bab5bd587fde68c06d8cc74bb986
Alexandre Nguyen <alex.nr@hotmail.fr>
2016-09-24 16:46:39 +0200

    bugfix

--- postcall/rainsensor.postcall.php
+++ postcall/rainsensor.postcall.php
@@ -5,9 +5,11 @@ require_once(__DIR__ . "/../class/RainSensor.class.php");
 if(isset($_POST)){
 
     $RainSenor = new RainSensor();
-    $RainSenor->sendRainingNotification();
 
     if(isset($_POST['stoppedRaining'])) {
         $RainSenor->sendStoppedRainingNotification();
+    }
+    else {
+        $RainSenor->sendRainingNotification();
     }
 }
