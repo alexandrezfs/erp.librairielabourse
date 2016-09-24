f676070ca554709728a57a91fc0b159baf1cea76
Alexandre Nguyen <alex.nr@hotmail.fr>
2016-09-24 13:10:38 +0200

    Bugfixes

--- class/RainSensor.class.php
+++ class/RainSensor.class.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once(__DIR__ . "/../autoload/db.autoload.php");
-require_once(__DIR__ . "/../class/IosPushNotificationCenter.class.php");
+require_once(__DIR__ . "/../class/IosPushNotificationCenter.php");
 
 class RainSensor {
 
     private $iosNotificationCenter;
 
-    public function sensRainingNotification() {
+    public function sendRainingNotification() {
 
-        $this->iosNotificationCenter = new IosPushNotificationCenter();
+        new IosPushNotificationCenter();
         $this->iosNotificationCenter->broadcastNotification("Pluie détectée au magasin ! Prenez les mesures nécéssaires.");
     }
 }
--- postcall/rainsensor.postcall.php
+++ postcall/rainsensor.postcall.php
@@ -5,6 +5,6 @@ require_once(__DIR__ . "/../class/RainSensor.class.php");
 if(isset($_POST)){
 
     $RainSenor = new RainSensor();
-    $RainSenor->sensRainingNotification();
+    $RainSenor->sendRainingNotification();
 
 }
